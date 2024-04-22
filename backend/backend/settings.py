60482a8caed1fae8cd7792783c1abfd64c3ad391
Nebula <nebulaanish@gmail.com>
2024-04-22 22:43:17 +0545

    Feat : Setup postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,12 +67,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog', 
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
