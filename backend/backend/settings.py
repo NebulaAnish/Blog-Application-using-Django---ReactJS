dc0c9fc8ea15a2a9c882d0a292bcfb2e281b2b65
Nebula <nebulaanish@gmail.com>
2024-04-22 20:54:21 +0545

    Feat : register new model to admin panel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,3 +112,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom Additions
+AUTH_USER_MODEL = "users.CustomUser"
