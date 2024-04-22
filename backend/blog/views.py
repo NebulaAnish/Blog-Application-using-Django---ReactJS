455058b1684e5669be651db847ebf6142e59eb59
Nebula <nebulaanish@gmail.com>
2024-04-22 23:07:23 +0545

    feat: implement pagination

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,12 +1,19 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from .models import Blog
 from .serializers import BlogSerializer
+
 
 class BlogListCreateAPIView(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+    
+    # To save the current user as author
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 class BlogRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
